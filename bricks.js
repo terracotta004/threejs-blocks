eb40be0599bf4210242a3266ab4af55282bce992
Ben Underwood <bu1157@messiah.edu>
2018-01-27 16:36:33 -0500

    some nubs

--- bricks.js
+++ bricks.js
@@ -37,7 +37,7 @@ window.onload = function () {
     scene.add(light);
 
     let light2 = new THREE.PointLight(0xffffff, 0.5);
-
+    // light2.position.set(0, 0, 0);
     scene.add(light2);
 
     let geometry = new THREE.BoxGeometry( 10, 5, 10 );
@@ -49,19 +49,55 @@ window.onload = function () {
     // let material = new THREE.MeshToonMaterial();
 
     let mesh = new THREE.Mesh(geometry, material);
-    mesh.position.z = -100;
-    mesh.rotation.z = 0.25;
-    mesh.rotation.y = 0.25;
-    mesh.rotation.x = 0.25;
+
+    let brick = new THREE.Group();
+    brick.add(mesh);
+
+    geometry = new THREE.CylinderGeometry( 1.25, 1.25, 2, 32);
+    material = new THREE.MeshLambertMaterial( {color: 0x00ff00});
+
+    let cylinder = new THREE.Mesh(geometry, material);
+
+    let nubs = [];
+
+    for (let i = 0; i < 6; i++) {
+      nubs[i] = new THREE.Mesh(geometry, material);
+    }
+
+    nubs[0].position.y += 2.5;
+    nubs[0].position.z += 3;
+    nubs[0].position.x += 3;
+    brick.add(nubs[0]);
+
+    nubs[1].position.y += 2.5;
+    nubs[1].position.z += 3;
+    nubs[1].position.x += 0;
+    brick.add(nubs[1]);
+
+    nubs[2].position.y += 2.5;
+    nubs[2].position.z += 3;
+    nubs[2].position.x += -3;
+    brick.add(nubs[2]);
+
+    brick.position.z = -100;
+    brick.rotation.z = 0.25;
+    brick.rotation.y = 0.25;
+    brick.rotation.x = 0.25;
 
     // let pivot = new THREE.Group();
-    scene.add(mesh);
+    scene.add(brick);
     // scene.add(pivot);
     // camera.position.set(300, 30, 900);
 
-    const animate = function () {
+    let rotationLabel = document.createElement('h1');
+    rotationLabel.innerHTML = brick.rotation.z;
+    rotationLabel.id = "rotationLabel";
+    document.body.appendChild(rotationLabel)
 
-      mesh.rotation.z += 0.01;
+    const animate = function () {
+      // console.log(brick.rotation.z);
+      rotationLabel.innerHTML = "Z Rotation: " + brick.rotation.z;
+      // brick.rotation.z += 0.01;
       // mesh.rotation.y += 0.25;
       // mesh.rotation.x += 0.25;
 
